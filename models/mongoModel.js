62a6f48aa37c96445d8f35f67b958c9e0145a8ac
Binit Shrestha <contact@finnep.fi>
2024-03-09 13:53:17 +0200

    initial commit

---
+++ models/mongoModel.js
@@ -0,0 +1,112 @@
+'use strict'
+
+(function () {
+    const mongoose = require('mongoose')
+	const Schema = mongoose.Schema;
+	const bcrypt = require('bcrypt')
+	require('dotenv').config()
+
+    let root = typeof exports !== "undefined" && exports !== null ? exports : window
+
+    //user role is defined here
+	const roleSchema = new Schema({
+		roleType: { type: String, unique: true },
+		createdAt: { type: Date, default: Date.now }
+	});
+
+	const Role = mongoose.model('Role', roleSchema);
+	root.Role = Role;
+
+
+	const userSchema = new Schema({
+		name: { type: String, required: true, unique: true, immutable: true, trim: true },
+		pwd: { type: String, required: true },
+		role: { type: mongoose.Schema.Types.ObjectId, ref: 'Role' },
+		active: { type: Boolean, default: true },
+		notificationAllowed:{ type: Boolean, default: true },
+		createdAt: { type: Date, default: Date.now }
+	});
+
+
+	userSchema.pre('save', function (next) {
+		let user = this;
+		bcrypt.genSalt(Number(process.env.SALT_WORK_FACTOR), function (err, salt) {
+			if (err) {
+				return next(err);
+			} else {
+				bcrypt.hash(user.pwd, salt, function (err, hash) {
+					if (err) return next(err);
+					user.pwd = hash;
+					next();
+				});
+			}
+		});
+	});
+
+	userSchema.methods.hashPassword = async function (userPassword) {
+		const hashPassword = await bcrypt.genSalt(Number(process.env.SALT_WORK_FACTOR)).then(salt => {
+			if (salt) {
+				return bcrypt.hash(userPassword, salt).then(hash => {
+					return hash
+				})
+			}
+		}).catch(err => { return err })
+		return hashPassword
+
+	}
+	userSchema.methods.comparePassword = async function (pwd) {
+		return await bcrypt.compare(pwd, this.pwd);
+	}
+
+	const User = mongoose.model('User', userSchema)
+	root.User = User
+
+    //encryption
+	const CryptoSchema = new mongoose.Schema({
+		createdAt: { type: Date, default: Date.now },
+		iv: { type: String, required: true },
+		encryptedData: { type: String, required: true },
+		type: { type: String, required: true }
+	})
+
+	const Crypto = mongoose.model('Crypto', CryptoSchema)
+	root.Crypto = Crypto
+    
+	// contact schema
+	const contactSchema = new Schema({
+		user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+		crypto: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Crypto' }],
+		streetName: { type: String, required: true },
+		createdAt: { type: Date, default: Date.now }
+	});
+	const Contact = mongoose.model('Contact', contactSchema);
+	root.Contact = Contact
+
+    /**************** Event Type schema ******************** */
+	const eventTypeSchema = new mongoose.Schema({
+
+		eventType: { type: String, required: true, unique: true },
+		position: { type: Number, required: true },
+		lang:{type:String, default:'en'},
+		createdAt: { type: Date, default: Date.now }
+	})
+
+    const EventType = mongoose.model('EventType', eventTypeSchema)
+	root.FoodType = ErrorEventType
+
+    /********************** Events ************************/
+	const eventSchema = new mongoose.Schema({
+		eventTitle: { type: String, required: true },
+		eventDescription: { type: String, required: true, unique: true },
+		eventTime: { type: Number, required: true }, 
+		eventDate: { type: Date, required: true },
+		eventPrice: { type: mongoose.Types.Decimal128, required: true },
+		lang:{type:String, default:'en'},
+		position:{type:Number},
+		createdAt: { type: Date, default: Date.now }
+	})
+
+	const Event = mongoose.model('Event', eventSchema)
+	root.Event = Event
+
+}).call(this)
